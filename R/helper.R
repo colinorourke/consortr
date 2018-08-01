0858dd165c447c75f674ec425df8a39296d8fd72
Colin O'Rourke <COrourke@immunetolerance.org>
2018-08-01 15:23:58 -0700

    Initial commit of package

---
+++ R/helper.R
@@ -0,0 +1,21 @@
+#' Run R code in temporary working directory.
+#'
+#' Sometimes one might want to run R code with a temporary working directory in
+#' effect.  This function will do that
+#'
+#' @param dir Scalar string giving the desired temporary working directory.
+#' @param expr Expression to run with the temporary working directory in effect.
+#'
+#' @return Invisible NULL
+#' @import checkmate
+#'
+#' @examples
+#' with_dir(tempdir(), cat("Hello world!", file=tempfile(fileext=".txt")))
+with_dir = function(dir, expr){
+  assertDirectoryExists(dir)
+  old_wd = getwd()
+  on.exit(setwd(old_wd))
+  setwd(dir)
+  evalq(expr)
+  return(invisible())
+}
